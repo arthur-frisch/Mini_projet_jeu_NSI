038e21ed6e59a3f3869476a9de82afb743d057a6
Frisch Arthur <arthur_frisch@hotmail.com>
2022-05-13 13:25:54 +0200

    Modif structure code

--- gui.py
+++ gui.py
@@ -2,35 +2,24 @@ import tkinter as tk
 from random import randint
 
 
-class fenetre:
+class fenetre(tk.Tk):
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Trivial Poursuit")
-        self.window.geometry("1100x900")
-        self.bg = Bg(self.window)
-        self.frame = Frame(self.window)
-        self.window.mainloop()
-
-class Button:
-    def __init__(self, window, _text : str, _command=None):
-        self.button = tk.Button(window, text=_text, fg="black", command=_command)
-        self.button.pack()
-
-        
-class Bg:
-    def __init__(self, frame):
+        super().__init__()
+        self.title("Jeu !")
+        self.geometry("1100x800")
         self.bg = tk.PhotoImage(file="plateau.png")
-        self.label = tk.Label(frame, image=self.bg)
-        self.label.pack()
+        self.label_test = tk.Label(self, image=self.bg)
+        self.label_test.place(x=0, y=0)
+        self.quit_button= tk.Button(self, text="Quitter", command=self.destroy)
+        self.quit_button.place(x=350, y=750)
+        self.frame_right_button = tk.Frame(self)
+        self.frame_right_button.place(x=750, y=350)
+        self.alea_button = tk.Button(self.frame_right_button, text="Test", command=self.destroy)
+        self.alea_button.pack()
+        self.alea_button2 = tk.Button(self.frame_right_button, text="Test", command=self.destroy)
+        self.alea_button2.pack()
+        self.mainloop()
         
-class Frame:
-    def __init__(self, window):
-        self.frame = tk.Frame(window)
-        self.frame.pack()
-        self.button = Button(self.frame, "Destruction", window.destroy)
-        
-    def test(self):
-        print("Test ")
-    
+
         
 fenetre()
